5744b0762e8de4ed43bf254207795b98e4d6840a
Théo Juzen <tjuzen@e-r4-p6.s19.be>
2019-09-24 16:14:12 +0200

    added subjects for lvl 4

--- My_exam/4/brainfuck.c
+++ My_exam/4/brainfuck.c
@@ -11,13 +11,46 @@
 /* ************************************************************************** */
 
 
+// Assignment name  : brainfuck
+// Expected files   : *.c, *.h
+// Allowed functions: write, malloc, free
+// --------------------------------------------------------------------------------
+//
+// Write a Brainfuck interpreter program.
+// The source code will be given as first parameter.
+// The code will always be valid, with no more than 4096 operations.
+// Brainfuck is a minimalist language. It consists of an array of bytes
+// (in our case, let's say 2048 bytes) initialized to zero,
+// and a pointer to its first byte.
+//
+// Every operator consists of a single character :
+// - '>' increment the pointer ;
+// - '<' decrement the pointer ;
+// - '+' increment the pointed byte ;
+// - '-' decrement the pointed byte ;
+// - '.' print the pointed byte on standard output ;
+// - '[' go to the matching ']' if the pointed byte is 0 (while start) ;
+// - ']' go to the matching '[' if the pointed byte is not 0 (while end).
+//
+// Any other character is a comment.
+//
+// Examples:
+//
+// $>./brainfuck "++++++++++[>+++++++>++++++++++>+++>+<<<<-]
+// >++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." | cat -e
+// Hello World!$
+// $>./brainfuck "+++++[>++++[>++++H>+++++i<<-]>>>++\n<<<<-]>>--------.>+++++.>." | cat -e
+// Hi$
+// $>./brainfuck | cat -e
+// $
+
 #include <unistd.h>
 #include <stdlib.h>
 
 char    *par(char *src, int way)
 {
     int s;
-    
+
     s = 0;
     while (1)
     {
@@ -58,7 +91,7 @@ int        main(int ac, char **av)
 {
     char buf[2048];
     int a;
-    
+
     if (ac > 1)
     {
         a = 0;
--- My_exam/4/ft_split.c
+++ My_exam/4/ft_split.c
@@ -11,6 +11,21 @@
 /* ************************************************************************** */
 
 
+// Assignment name  : ft_split
+// Expected files   : ft_split.c
+// Allowed functions: malloc
+// --------------------------------------------------------------------------------
+//
+// Write a function that takes a string, splits it into words, and returns them as
+// a NULL-terminated array of strings.
+//
+// A "word" is defined as a part of a string delimited either by spaces/tabs/new
+// lines, or by the start/end of the string.
+//
+// Your function must be declared as follows:
+//
+// char    **ft_split(char *str);
+
 #include <stdlib.h>
 
 char        **ft_split(char *str)
@@ -19,7 +34,7 @@ char        **ft_split(char *str)
     int        j;
     int        k;
     char    **split;
-    
+
     i = 0;
     k = 0;
     if (!(split = (char **)malloc(sizeof(char *) * 256)))
